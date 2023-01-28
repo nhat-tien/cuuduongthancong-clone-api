08da54f8783c847dd0736252c48b8883b1f3abbc
Nhat Tien <ledangnhattien04@gmail.com>
2023-01-28 10:46:57 +0700

    add CORS enabled

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 const port = process.env.port || 3000;
 const data = require("./data.json");
 
+app.all('/api', (req,res,next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    next();
+});
+
 app.get("/api", (req,res) => {
     res.json(data);
 });
